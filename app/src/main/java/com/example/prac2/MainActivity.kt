22a322cf1a29548940c2b0e8b5c9200a0f146302
hjn971 <hjnkth971@gmail.com>
2020-10-29 13:21:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/prac2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.prac2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        R.id.bt_action
+
+
+    }
+}
